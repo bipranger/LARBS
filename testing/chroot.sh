464f2be7bde4b094a767e22bde19feab993b2a2f
bipranger <linknaoezelda-q@yahoo.com.br>
2019-04-07 12:00:46 -0300

    modified:   larbs.sh 	modified:   progs.csv 	modified:   testing/arch.sh 	modified:   testing/chroot.sh

--- testing/chroot.sh
+++ testing/chroot.sh
@@ -8,9 +8,9 @@ ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 
 hwclock --systohc
 
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "en_US ISO-8859-1" >> /etc/locale.gen
+echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
+echo "pt_BR ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
 pacman --noconfirm --needed -S networkmanager
@@ -20,5 +20,5 @@ systemctl start NetworkManager
 pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman --noconfirm --needed -S dialog
-larbs() { curl -O https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/larbs.sh && bash larbs.sh ;}
+larbs() { curl -O https://raw.githubusercontent.com/bipranger/LARBS/master/larbs.sh && bash larbs.sh ;}
 dialog --title "Install Luke's Rice" --yesno "This install script will easily let you access Luke's Auto-Rice Boostrapping Scripts (LARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nLuke"  15 60 && larbs
